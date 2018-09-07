d371440be69fb77ed5cccbc333afee9d1e543d1d
elizzzabethrae <elizabethwarners@gmail.com>
2018-09-07 16:39:19 -0400

    still working on submit

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -11,6 +11,23 @@ class RoomList extends Component {
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
+  handleChange(e) {
+    const newState = {
+      newRoom: e.target.value
+    }
+    this.setState(newState);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newRoom) { return }
+    const newRoomName = { name: this.state.newRoom };
+    this.setState({ rooms: [...this.state.rooms, newRoomName], newRoom: '' });
+    this.roomsRef.push({
+    name: newRoomName
+   });
+  }
+
     componentDidMount() {
       this.roomsRef.on('child_added', snapshot => {
         const room = snapshot.val();
@@ -19,24 +36,15 @@ class RoomList extends Component {
       });
     }
 
-
-    handleSubmit(e) {
-      e.preventDefault();
-      this.roomsRef.push({
-      name: newRoomName
-     });
-   }
-
-
     render() {
       return (
         <div className = "App">
-          <section className = 'listOfRooms'>
+          <section className = 'newRoom'>
             {this.state.rooms.map( (room) =>
             <div> {room.name} </div> )}
           </section>
           <form onSubmit={ (e) => this.handleSubmit(e) }>
-            <input type="text"/> value= {  this.state.newRoomName } />
+            <input type="text" value= {  this.state.newRoom } onChange={ (e) => this.handleChange(e) } />
             <input type="submit" />
           </form>
         </div>
